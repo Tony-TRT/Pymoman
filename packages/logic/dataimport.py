e83a90f6deab716a6867824dadb1d1470d61a0bf
Tony-TRT <146631446+Tony-TRT@users.noreply.github.com>
2023-10-31 21:21:56 +0100

    Update dataimport module.

--- packages/logic/dataimport.py
+++ packages/logic/dataimport.py
@@ -9,6 +9,14 @@ from .movie import Movie
 from ..constants import constants
 
 
+def load_file_content(input_file):
+    """Loads a json file and returns its content
+    """
+
+    with open(input_file, 'r', encoding="UTF-8") as file:
+        return json.load(file)
+
+
 def load_actors_list() -> list[str]:
     """Retrieves all actors from data.json files
 
@@ -20,10 +28,9 @@ def load_actors_list() -> list[str]:
 
     for file_path in constants.CACHE.glob('**/data.json'):
         try:
-            with open(file_path, 'r', encoding="UTF-8") as data_file:
-                content = json.load(data_file)
-                actors = content.get('actors', ['Unknown'])
-                full_list.update(actors)
+            content = load_file_content(file_path)
+            actors = content.get('actors', ['Unknown'])
+            full_list.update(actors)
         except json.JSONDecodeError:
             continue
 
@@ -43,16 +50,15 @@ def load_movies() -> list[Movie]:
 
     for file_path in constants.COLLECTIONS.glob('**/*.json'):
         try:
-            with open(file_path, 'r', encoding="UTF-8") as data_file:
-                content = json.load(data_file)
-                for stored_movie in content:
-                    mov = Movie(
-                        title=stored_movie.get('title'),
-                        year=stored_movie.get('year'),
-                        path=stored_movie.get('path'),
-                        rating=stored_movie.get('rating')
-                    )
-                    full_list.append(mov)
+            content = load_file_content(file_path)
+            for stored_movie in content:
+                mov = Movie(
+                    title=stored_movie.get('title'),
+                    year=stored_movie.get('year'),
+                    path=stored_movie.get('path'),
+                    rating=stored_movie.get('rating')
+                )
+                full_list.append(mov)
         except json.JSONDecodeError:
             continue
 
