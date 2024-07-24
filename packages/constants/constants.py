1c325485fbc4de3c63b268f5b1e78c7d4fd694ba
Tony-TRT <146631446+Tony-TRT@users.noreply.github.com>
2024-07-24 17:29:03 +0200

    Refactor 'constants.py' module.

--- packages/constants/constants.py
+++ packages/constants/constants.py
@@ -9,52 +9,26 @@ from pathlib import Path
 
 
 BASE: final(Path) = Path(__file__).resolve().parent.parent.parent
-APP_HIDDEN_FOLDER: final(Path) = Path.joinpath(Path.home(), '.pymoman')
+APP_HIDDEN_FOLDER: final(Path) = Path.joinpath(Path.home(), ".pymoman")
 
-STR_PATHS = {}
 PATHS: final(dict) = {
-    'settings': Path(APP_HIDDEN_FOLDER / "settings.json"),
-    'cache': Path(APP_HIDDEN_FOLDER / "cache"),
-    'collections': Path(APP_HIDDEN_FOLDER / "collections"),
-    'recommendations': Path(APP_HIDDEN_FOLDER / "recommendations"),
-    'resources': Path(BASE / "resources"),
-    'default font': Path(BASE / "resources" / "fonts" / "default.ttf"),
-    'cyber font': Path(BASE / "resources" / "fonts" / "cyber.ttf"),
-    'default poster': Path(BASE / "resources" / "posters" / "default.jpg"),
-    'wishlist': Path(BASE / "resources" / "posters" / "wishlist.jpg"),
-    'default style': Path(BASE / "resources" / "styles" / "default.qss"),
-    'cyber style': Path(BASE / "resources" / "styles" / "cyber.qss"),
-    'icons': Path(BASE / "resources" / "icons")
+    "settings": Path(APP_HIDDEN_FOLDER / "settings.json"),
+    "cache": Path(APP_HIDDEN_FOLDER / "cache"),
+    "collections": Path(APP_HIDDEN_FOLDER / "collections"),
+    "recommendations": Path(APP_HIDDEN_FOLDER / "recommendations"),
+    "resources": Path(BASE / "resources"),
+    "default font": Path(BASE / "resources" / "fonts" / "default.ttf"),
+    "cyber font": Path(BASE / "resources" / "fonts" / "cyber.ttf"),
+    "default poster": Path(BASE / "resources" / "posters" / "default.jpg"),
+    "wishlist": Path(BASE / "resources" / "posters" / "wishlist.jpg"),
+    "default style": Path(BASE / "resources" / "styles" / "default.qss"),
+    "cyber style": Path(BASE / "resources" / "styles" / "cyber.qss"),
+    "icons": Path(BASE / "resources" / "icons")
 }
+STR_PATHS: final(dict) = {key: str(value) for key, value in PATHS.items()}
 
-for key, value in PATHS.items():
-    STR_PATHS[key] = str(value)
-
-STR_ICONS = {}
-ICONS: final(dict) = {
-    'logo': Path(PATHS.get('icons') / "logo.png"),
-    'save': Path(PATHS.get('icons') / "save.png"),
-    'note': Path(PATHS.get('icons') / "create_collection.png"),
-    'folder': Path(PATHS.get('icons') / "folder.png"),
-    'collection': Path(PATHS.get('icons') / "collection.png"),
-    'movie': Path(PATHS.get('icons') / "movie.png"),
-    'add': Path(PATHS.get('icons') / "add_item.png"),
-    'rem': Path(PATHS.get('icons') / "remove_item.png"),
-    'search': Path(PATHS.get('icons') / "search.png"),
-    'previous': Path(PATHS.get('icons') / "previous.png"),
-    'export': Path(PATHS.get('icons') / "export.png"),
-    'delete': Path(PATHS.get('icons') / "delete.png"),
-    'official': Path(PATHS.get('icons') / "official.png"),
-    'star': Path(PATHS.get('icons') / "star.png"),
-    'trailer': Path(PATHS.get('icons') / "trailer.png"),
-    'imdb': Path(PATHS.get('icons') / "imdb.png"),
-    'wishlist': Path(PATHS.get('icons') / "wishlist.png"),
-    'new_poster': Path(PATHS.get('icons') / "new_poster.png"),
-    'default_poster': Path(PATHS.get('icons') / "default_poster.png")
-}
-
-for key, value in ICONS.items():
-    STR_ICONS[key] = str(value)
+ICONS: final(dict) = {icon_path.stem: icon_path for icon_path in PATHS["icons"].iterdir()}
+STR_ICONS: final(dict) = {key: str(value) for key, value in ICONS.items()}
 
 MOVIE_GENRES: final(dict) = {
     "action": "action",
